e356b17a7aaaad0b8919c5a5df9698a8df88a75e
Hajime Iyama <Hajime.Iyama@ibm.com>
2023-04-06 14:19:13 +0900

    Initial commit

---
+++ server.js
@@ -0,0 +1,104 @@
+// const AUTHINFO = {
+//    username:"test",
+//    password:"test"
+// };
+
+const express = require('express');
+// const asyncHandler = require('express-async-handler');
+
+// File upload function
+const fs = require('fs');
+// const multer = require('multer');
+// const upload = multer({dest: 'tmp/'});
+
+// const basicAuth = require('basic-auth-connect');
+
+const cors = require('cors');
+require('dotenv').config({ debug:true });
+
+const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+const { IamAuthenticator } = require('ibm-watson/auth');
+
+if (typeof process.env.API_KEY == 'undefined') {
+   console.error('Error: "API_KEY" is not set.');
+   console.error('Please consider adding a .env file with API_KEY.');
+   process.exit(1);
+}
+
+const nluAuthenticator = new IamAuthenticator({
+   apikey: process.env.API_KEY,
+});
+
+const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+  version: '2022-04-07',
+  authenticator: nluAuthenticator,
+  serviceUrl: process.env.API_BASE_URL,
+});
+
+var server = express();
+server
+   .use(express.urlencoded({extended: false}))
+   .use(express.json())
+
+   // CORS
+   .use(cors())
+
+   // Basic Auth
+   // .use(basicAuth(
+   //    AUTHINFO.username,
+   //    AUTHINFO.password 
+   // ))
+   .use(express.static('public'))
+
+
+// Auth
+   // token 
+   .get('/token', function (req, res) {
+      if (sttAuthenticator) {
+         sttAuthenticator.tokenManager.requestToken()
+             .then(({result}) =>{
+               // console.log(result);
+               res.json({accessToken: result.access_token});
+             })
+      } else {
+         res.status(500).send({"error": "No AccessToken."});
+      }
+   })
+
+// Analyze
+   .post('/analyze', function (req, res) {
+      let param = req.body;
+      // Check Params
+      if (!(param && param.features)) {
+         res.status(500).send({"error": "Invalid params!"});
+         return;
+      }
+
+      // const analyzeParams = {
+      //    'url': 'https://en.wikipedia.org/wiki/Emmanuel_Macron',
+      //    'features': {
+      //       'sentiment': {
+      //          'targets': [
+      //             'France'
+      //          ]
+      //       }
+      //    }
+      // };
+
+      naturalLanguageUnderstanding.analyze(param)
+      .then(analysisResults => {
+         console.log(JSON.stringify(analysisResults, null, 2));
+         res.json(analysisResults.result);
+      })
+      .catch(err => {
+         console.log('error:', err);
+         res.status(500).send({"error": err});
+      });
+   })
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log('Watson NLU Bridge Server running on port: %d', port);
+//   console.log('process.env: ', process.env);
+});
